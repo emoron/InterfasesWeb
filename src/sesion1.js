61579fdfefb55550be4772abfe65a74450b87be7
Emoron <emoron@gmail.com>
2016-02-10 14:03:06 -0600

    / ‘src/sesion1.js’

--- src/sesion1.js
+++ src/sesion1.js
@@ -10,22 +10,3 @@ opposite = true
 square = (x) -> x * x
 cube = (x) -> square(x) * x
 cube(10)
-
-// Arrays:
-list = [1, 2, 3, 4, 5]
-
-// Objects:
-math =
-  root:   Math.sqrt
-  square: square
-  cube:   (x) -> x * square x
-
-// Splats:
-race = (winner, runners...) ->
-  print winner, runners
-
-// Existence:
-//alert "I knew it!" if elvis?
-
-// Array comprehensions:
-cubes = (math.cube num for num in list)
